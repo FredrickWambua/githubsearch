dce080935066186bc2a4a0403c3d90c570fcf286
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-24 18:18:09 +0300

    creates user and repository classes

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
+  private base_url_user_search:string = "https://api.github.com/users/";
+  private userQuery:string = "FredrickWambua";
+  private reqUrl: string = this.base_url_user_search + this.userQuery +'/'
 
   constructor() { }
 }
