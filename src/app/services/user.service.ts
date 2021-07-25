2607d94d9ebb53eba165f8b25f09e05407d95d4e
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-25 11:04:12 +0300

    imports HttpClient module and initialise getProfile method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private base_url_user_search:string = "https://api.github.com/users/";
-  private userQuery:string = "FredrickWambua";
-  private reqUrl: string = this.base_url_user_search + this.userQuery +'/'
+  // private base_url_user_search:string = "https://api.github.com/users/";
+  // private userQuery:string = "FredrickWambua";
+  // private reqUrl: string = this.base_url_user_search + this.userQuery +'/'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  //for github profile
+  public getProfile(){
+    let dataUrl
+  }
 }
