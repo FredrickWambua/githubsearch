41965559c282506a0b8d36fe7e67cee7787e9cd3
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-25 11:05:19 +0300

    adds the api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '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'
 };
 
 /*
